30180befab8498c3f02780800ba39a249d14caee
PhanTien234 <minhtienp328@gmail.com>
2023-01-01 15:53:38 +0700

    Declare variable for Books and set up to connect with MongoDB

--- Bookstore.Core/Book.cs
+++ Bookstore.Core/Book.cs
@@ -1,6 +1,14 @@
-﻿namespace Bookstore.Core;
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Bookstore.Core;
 
 public class Book
 {
-    
+    [BsonId]
+    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string Category { get; set; }
+    public string Author { get; set; }
 }
