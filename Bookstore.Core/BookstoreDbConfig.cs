03a98d7e34a488640a8e4de09e4800ff97b08cc0
PhanTien234 <minhtienp328@gmail.com>
2023-01-01 17:35:20 +0700

    Push database to Client MangoDb

--- Bookstore.Core/BookstoreDbConfig.cs
+++ Bookstore.Core/BookstoreDbConfig.cs
@@ -3,6 +3,6 @@
 public class BookstoreDbConfig
 {
     public string Database_Name { get; set; }
-    public string Books_Collection_name { get; set; }
+    public string Books_Collection_Name { get; set; }
     public string Connection_String { get; set; }
 }
---
+++ Bookstore.Core/DbCLient.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Bookstore.Core;
+
+public class DbCLient : IDbClient
+{
+    private readonly IMongoCollection<Book> _books;
+    public DbCLient(IOptions<BookstoreDbConfig> bookstoreDbConfig)
+    {
+        var client = new MongoClient(bookstoreDbConfig.Value.Connection_String);
+        var database = client.GetDatabase(bookstoreDbConfig.Value.Database_Name);
+        _books = database.GetCollection<Book>(bookstoreDbConfig.Value.Books_Collection_Name);
+    }
+
+    public IMongoCollection<Book> GetBooksCollection() => _books;
+}
